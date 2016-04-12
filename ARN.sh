76b7c8d6fe72e338c71bac69b3d9639ecdff2c3f
Alex Taber <alex@ahsraiderreview.com>
2016-04-11 20:55:18 -0400

    Fixed some stuff

--- ARN.sh
+++ ARN.sh
@@ -20,7 +20,7 @@ rm -rf ./out #Delete the release folder
 mv ARN*.zip ../../builds #Move the zipped release to the builds directory
 cd /home/alex/AuReiNand/ #switch to the root of the directory
 cat current.html > /tmp/tmpcur #Copy the current list of table elements
-echo "<tr><td><p><a href="/builds/ARN-$(date +"%Y-%m-%d").zip">ARN-$(date +"%Y-%m-%d").zip</a></p></td><td>${commit}</td><td>$(date +"%Y-%m-%d")</td><td>${message}</td></tr>" > current.html #Add a new table data element to the top of the list
+echo "<tr><td><p><a href="/AuReiNand/builds/ARN-$(date +"%Y-%m-%d").zip">ARN-$(date +"%Y-%m-%d").zip</a></p></td><td>${commit}</td><td>$(date +"%Y-%m-%d")</td><td>${message}</td></tr>" > current.html #Add a new table data element to the top of the list
 cat /tmp/tmpcur >> current.html #Add the old table elements to the bottom of the list
 cat top.html > index.html #Copy the top half of the webpage to index
 cat current.html >> index.html #Copy the list of table elements to the index
