25305f202d8d69cf1426fffddd7ca549f16b7f74
Alex Taber <alex@ataber.pw>
2016-09-23 06:45:29 -0400

    Updated builds - Automated Commit Message

--- ARN.sh
+++ ARN.sh
@@ -23,7 +23,7 @@ echo $commit
 echo $(cat ../../lastCommit)
 echo $message
 echo $(cat ../../lastMessage)
-if [ "$commit" = "$(cat ../../lastCommit)" ] && [ "$message" = "$(cat ../../lastMessage)" ] && [ $newTag = false ]
+if [ "$commit" = "$(cat ../../lastCommit)" ] && [ "$message" = "$(cat ../../lastMessage)" ] && [ $newTag = true ]
 then
 	skipCheck=true
 fi
